338805b3275c0ea895dbd8865105f6e2e7c3cb64
오창민 <ochangmin@ochangmins-MacBook-Air.local>
2022-02-28 17:25:26 +0900

    사용자 아바타 보여주기

--- blog/models.py
+++ blog/models.py
@@ -75,4 +75,10 @@ class Comment(models.Model):
     def get_absolute_url(self):
         return f'{self.post.get_absolute_url()}#comment-{self.pk}'
 
+    def get_avatar_url(self):
+        if self.author.socialaccount_set.exists():
+            return self.author.socialaccount_set.first().get_avatar_url()
+        else:
+            return f'https://doitdjango.com/avatar/id/620/40a642ae9526e102/svg/{self.author.email}'
+
 
